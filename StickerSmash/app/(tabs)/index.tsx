c98449f726c572467d76e67128d8e76e7ea32161
PABLO_MANUEL_OTON_CARIDAD <pablootoncaridadzalaeta@gmail.com>
2025-09-17 12:08:31 +0200

    Añadir botones Home

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View, StyleSheet } from 'react-native';
+import {  View, StyleSheet } from 'react-native';
+import Button from '@/components/Button';
 import ImageViewer from '@/components/imageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -8,6 +9,10 @@ const Index = () => (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   
@@ -21,6 +26,10 @@ const styles = StyleSheet.create({
   },
    imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
   
 });
