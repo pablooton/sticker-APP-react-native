21b5038d67b25c1c830f9e29212c4ce4bfe89290
PABLO_MANUEL_OTON_CARIDAD <pablootoncaridadzalaeta@gmail.com>
2025-09-18 12:06:33 +0200

    Permitir seleccionar imagen

--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -3,10 +3,11 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
   label: string;
-   theme?: 'primary';
+  theme?: 'primary';
+  onPress?: () => void;
 };
 
-const Button = ({ label,theme }: Props) => {
+const Button = ({ label,theme,onPress }: Props) => {
     if (theme === 'primary') {
   return (
       <View
@@ -16,7 +17,8 @@ const Button = ({ label,theme }: Props) => {
         ]}>
         <Pressable
           style={[styles.button, { backgroundColor: '#fff' }]}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={onPress}>
+
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
--- StickerSmash/components/imageViewer.tsx
+++ StickerSmash/components/imageViewer.tsx
@@ -3,10 +3,15 @@ import { Image } from 'expo-image';
 
 type Props = {
   imgSource: ImageSourcePropType;
+  selectedImage?: string;
 };
 
-const ImageViewer = ({ imgSource }: Props) => <Image source={imgSource} style={styles.image} />;
+const ImageViewer = ({ imgSource, selectedImage }: Props) => 
+{
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
 
+  return <Image source={imageSource} style={styles.image} />;
+}
 
 const styles = StyleSheet.create({
   image: {
